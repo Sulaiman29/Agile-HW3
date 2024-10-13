8bbbf672fb09ff1f4259409235f2ded31df8df2d
Sulaiman Javed <mujave@taltech.ee>
2024-10-13 21:56:16 +0300

    lab handout#4 done

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Takso.{Repo, Accounts.User}
+
+[%{name: "Dio Brando", username: "DIO", password: "itsmedio"},
+ %{name: "Jotaro Kujo", username: "JoJo", password: "oraoraora"}]
+|> Enum.map(fn user_data -> User.changeset(%User{}, user_data) end)
+|> Enum.each(fn changeset -> Repo.insert!(changeset) end)
