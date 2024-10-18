13b4b777f05f10ce84f7558344c6d804f0808252
Sulaiman Javed <sulaimanjaved2000@gmail.com>
2024-10-19 00:10:57 +0300

    Part 1: Testing the controllers

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -4,4 +4,16 @@ defmodule TaksoWeb.BookingController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+   def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def create(conn, %{"booking" => _booking_params}) do
+    # At this moment, we simply return a success message without database interaction
+    # Return a response indicating success, without saving to the database
+    conn
+    |> put_flash(:info, "Your taxi will arrive in 2 minutes.")
+    |> redirect(to: ~p"/bookings")  # Redirect to index
+  end
 end
