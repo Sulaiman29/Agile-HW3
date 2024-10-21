33d52eea3424f6a9d83a8f6a8b4ca87d7231aae2
Sulaiman Javed <sulaimanjaved2000@gmail.com>
2024-10-21 20:49:34 +0300

    Part 3: Ecto queries

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -1,5 +1,8 @@
 defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
+  alias Takso.Repo
+  alias Takso.Sales.Taxi
+  import Ecto.Query, only: [from: 2]
 
   def new(conn, _params) do
     render conn, "new.html"
@@ -10,11 +13,21 @@ defmodule TaksoWeb.BookingController do
   end
 
   def create(conn, %{"booking" => %{"pickup_address" => pickup_address, "dropoff_address" => dropoff_address}}) do
-    # At this moment, we simply return a success message without database interaction
-    # Return a response indicating success, without saving to the database
-    conn
-    |> put_flash(:info, "Your taxi will arrive in 5 minutes")
-    |> redirect(to: ~p"/bookings")  # Redirect to index
+    # Query for available taxis
+    query = from(t in Taxi, where: t.status == "available", select: t)
+    available_taxis = Repo.all(query)
+
+    if length(available_taxis) > 0 do
+      # There are taxis available
+      conn
+      |> put_flash(:info, "Your taxi will arrive in 5 minutes")
+      |> redirect(to: ~p"/bookings")  # Redirect to index
+    else
+      # No taxis available
+      conn
+      |> put_flash(:error, "At present, there is no taxi available!")
+      |> redirect(to: ~p"/bookings")  # Redirect to index
+    end
   end
 
   def show(conn, %{"id" => _id}) do
